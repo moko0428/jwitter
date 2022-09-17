0a6599caa0d0cf910faac5c5e18527801e21eb60
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-09-17 21:57:09 +0900

    Using Firebase Auto error

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import fbase from "fbase";
+import authService from "../fbase";
 function App() {
-  return <AppRouter />;
+  console.log(authService.currentUser);
+  const auth = fbase.auth();
+  const [isLoggedIn, setIsLoggendIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Jwitter</footer>
+    </>
+  );
 }
 
 export default App;
