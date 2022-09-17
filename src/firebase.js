788f8bf9394125739ecde1dc13b958e907aed474
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-09-17 20:55:47 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEvzhEtOAEWIXRy5u6XGJxcf5tGaKPn-w",
+  authDomain: "jwitter-1a34d.firebaseapp.com",
+  projectId: "jwitter-1a34d",
+  storageBucket: "jwitter-1a34d.appspot.com",
+  messagingSenderId: "670958140017",
+  appId: "1:670958140017:web:3da16af004166156dab9d0",
+};
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import firebase from "firebase/compat/app";
+console.log(firebase);
 ReactDOM.render(
   <React.StrictMode>
     <App />
