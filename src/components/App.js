aa38319184aae14b2aa8b4a2e1c703e460d29863
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-09-27 21:53:04 +0900

    회원가입

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggendIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggendIn(true);
+      } else {
+        setIsLoggendIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
       <footer>&copy; {new Date().getFullYear()} Jwitter</footer>
     </>
   );
--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -11,5 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// const authService = firebase.getAuth();
-export default app;
+
+export const authService = getAuth();
