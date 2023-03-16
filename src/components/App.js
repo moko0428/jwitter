bbe1da56abe16ff3245da5275be043f8f5ee6040
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-03-16 23:09:43 +0900

    316

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
-import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { authService } from "fbase";
 
